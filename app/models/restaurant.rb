ea486e5ce633aac10e82dbef9be620c65d799081
Lou Welgryn <lou.welgryn@gmail.com>
2019-08-14 11:24:06 +0200

    push

---
+++ app/models/restaurant.rb
@@ -0,0 +1,6 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  CATEGORIES = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+  validates :category, inclusion: { in: CATEGORIES }
+  validates :name, :address, :category, presence: true
+end
---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  validates :restaurant_id, presence: true
+  validates :content, :rating, presence: true
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+  validates :rating, numericality: { only_integer: true }
+end
